b6d826dc032b9c35b178a0ad7ab02700c6d3319b
Prin Phunyaphibarn <prin10517@gmail.com>
2023-11-23 04:50:24 +0000

    hyperparemeter change

--- arguments/__init__.py
+++ arguments/__init__.py
@@ -82,10 +82,10 @@ class OptimizationParams(ParamGroup):
         self.percent_dense = 0.01
         self.lambda_dssim = 0.2
         self.densification_interval = 100
-        self.reflection_densification_interval = 100
+        self.reflection_densification_interval = 200
         self.opacity_reset_interval = 3000
         self.densify_from_iter = 500
-        self.reflection_densify_from_iter = 500
+        self.reflection_densify_from_iter = 1000
         self.densify_until_iter = 15_000
         self.densify_grad_threshold = 0.0002
         self.random_background = False
